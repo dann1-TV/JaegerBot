30a003b4423cf5f909712163ccc7595e1e7a37bc
Daniel Clavijo Coca <dclavijo@opennebula.io>
2023-03-18 09:29:12 -0600

    Fix CLI args

--- src/responses.py
+++ src/responses.py
@@ -2,35 +2,35 @@ import bot_basic
 import tarkov
 
 def get_response(message: str, tarkov_data: dict) -> str:
-    p_message = message.lower()[1:] # except the first char ? or !
+    command = message.lower()[1:] # except the first char ? or !
 
     # troll is the keyword and the rest of the maessage is trolled
-    if p_message[:5] == "troll":
-        return bot_basic.randomize_case(p_message[5:])
+    if command[:5] == "troll":
+        return bot_basic.randomize_case(command[5:])
 
-    if p_message == 'roll' or p_message == 'dice':
+    if command == 'roll' or command == 'dice':
         return bot_basic.dice()
 
-    if p_message == 'coinflip' or p_message == 'coin':
+    if command == 'coinflip' or command == 'coin':
         return bot_basic.coin()
 
-    if p_message == 'tip':
+    if command == 'tip':
         return tarkov.random_tip(tarkov_data['tips'])
 
     # TODO: Tag tips with PvP, PvE, money, hideout
     # TODO: Tip search
-    if p_message == 'map':
+    if command == 'map':
         return tarkov.random_map(tarkov_data['maps'])
 
     # EFT Money making playlist
-    if p_message == 'money':
+    if command == 'money':
         return "https://www.youtube.com/playlist?list=PLhxeo6sgtUlo3_2d8salSzhAz6gZcvbYa"
 
-    if p_message == 'about' or message == 'code' or message == 'source':
+    if command == 'about' or command == 'code' or command == 'source':
         return "https://github.com/dann1-TV/JaegerBot"
 
 
-    if p_message == 'help' or p_message == 'commands':
+    if command == 'help' or command == 'commands':
         return help()
 
     return help()
@@ -46,4 +46,6 @@ def help():
     - `map`                     prints a random map name
     - `money`                   How to make easy money
     - `about/source/code`       Developer information
+
+    Prepend a `!`/`?` to the command for a **public**/**private** reply
     """
